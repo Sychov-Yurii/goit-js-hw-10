c52555f678015fce97191615c68e56edb3019a0b
Sychov-Yurii <sychov.yurii@outlook.com>
2023-10-23 21:08:58 +0200

    update

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+function finderCats () {
+    const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
+    const API_KEY = 'live_vHQe36pHRTLs0QD5mvEAHeklHDj7vC1421nryMAZQDOGH4LbNKgPPgMpTLXw4t2t';
+
+return fetch(`${BASE_URL}`)
+}
