c921a45845a42b4bab470160f1923971f5a00c90
Sychov-Yurii <sychov.yurii@outlook.com>
2023-10-24 16:00:38 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_vHQe36pHRTLs0QD5mvEAHeklHDj7vC1421nryMAZQDOGH4LbNKgPPgMpTLXw4t2t";
+
 function finderCats () {
     const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
     const API_KEY = 'live_vHQe36pHRTLs0QD5mvEAHeklHDj7vC1421nryMAZQDOGH4LbNKgPPgMpTLXw4t2t';
 
-return fetch(`${BASE_URL}`)
+return fetch(`${BASE_URL}__${API_KEY}`).then(resp => {
+    console.log(resp);
+})
 }
